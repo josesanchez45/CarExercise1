07c41df7332c4fcc2ea65685261109b6757b2f28
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-18 09:31:16 -0700

    initial commit

---
+++ CarClassExercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using CarClassExercise1;
+
+Car firstCar = new Car()
+{
+   Make= "Ford",
+   Model =" Fusion",
+   CarYear = 2014
+};
+
+Console.WriteLine($"The car is a {firstCar.Make}");
+Console.WriteLine($"The Car model is {firstCar.Model}");
+Console.WriteLine($"It is a {firstCar.CarYear}");   
